afbe09e5e26173b34f50fe6e2f58ebb17ea2438b
Vinicius Tolotti Borba <e.vtolottiborba@etu.cvm.qc.ca>
2024-11-04 10:02:44 -0500

    db_v2

---
+++ Projet/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('erp.db')
+    conn.row_factory = sqlite3.Row  
+    return conn
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT visibilite FROM LOGIN WHERE username = ? AND password = ?", (username, password))
+    user = cursor.fetchone()
+    conn.close()
+    
+    if user:
+        return jsonify({'success': True, 'role': user['visibilite']})
+    else:
+        return jsonify({'success': False}), 401
+
+if __name__ == '__main__':
+    app.run(debug=True)
